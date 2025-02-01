34cc1eaf4c2f398fab9bd5bbaa13961e7df1f8ab
league <league@imac-39.attlocal.net>
2025-01-31 18:30:43 -0800

    wasd

--- src/_03_gui_with_help/_5_duelling_buttons/DuellingButtons.java
+++ src/_03_gui_with_help/_5_duelling_buttons/DuellingButtons.java
@@ -6,12 +6,14 @@ package _03_gui_with_help._5_duelling_buttons;
  */
 
 import java.awt.Dimension;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 
 public class DuellingButtons implements ActionListener {
 
@@ -24,28 +26,29 @@ public class DuellingButtons implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 
+
 	public void run() {
 
 		// 1. Add the panel to the frame
 			frame.add(panel);
 		// 2. Make the frame visible
-			frame.isVisible(true);
+			frame.isVisible();
 		// 3. Set the text of the leftButton to "Click me!"
-
+			leftButton.setText("Click me!");
 		// 4. Set the text of the rightButton to "Click me!"
-
+			rightButton.setText("Click me!");
 		// 5. Add an action listener to the leftButton
-
+			leftButton.addActionListener(null);
 		// 6. Add an action listener to the rightButton
-
+			rightButton.addActionListener(null);
 		// 7. Add the leftButton to the panel
-
+			panel.add(leftButton);
 		// 8. Add the rightButton to the panel
-
+			panel.add(rightButton);
 		// 9. Pack the frame
-
+			frame.pack();
 		// 10. Set the title of the frame to "Demanding Buttons"
-
+			frame.setTitle("Demanding Buttons");
 	}
 
 	@Override
---
+++ src/_04_gui_from_scratch/_1_fortune_cookie/fortune_cookie.java
@@ -0,0 +1,5 @@
+package _04_gui_from_scratch._1_fortune_cookie;
+
+public class fortune_cookie {
+
+}
