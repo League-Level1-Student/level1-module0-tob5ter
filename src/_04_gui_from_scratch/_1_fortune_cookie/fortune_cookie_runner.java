aade9c987e56380486c3e64c9e8a796bb7f4f992
whale92130 <alexander92130@gmail.com>
2025-02-07 18:27:00 -0800

    asfjdkl;a

--- src/_04_gui_from_scratch/_1_fortune_cookie/fortune_cookie_runner.java
+++ src/_04_gui_from_scratch/_1_fortune_cookie/fortune_cookie_runner.java
@@ -1,5 +1,28 @@
 package _04_gui_from_scratch._1_fortune_cookie;
 
-public class fortune_cookie_runner {
+import java.awt.Dimension;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class fortune_cookie_runner {
+	JFrame frame = new JFrame();
+	JButton button = new JButton();
+	int rand = new Random().nextInt(5);
+	public void actionPerformed() {
+		JOptionPane.showMessageDialog(null, "WooHoo!");
+	}
+	public void showButton() {
+        frame.setVisible(true);
+        frame.add(button);
+        frame.pack();
+        button.addActionListener(null);
+        
+   }
 }
